9b621c97731e3ef89e1ae1c9239950e5fd90c9e4
Torres-Alan <adtorresfuentes14@gmail.com>
2026-04-02 17:15:12 -0600

    Add task functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+
+
+    let li = document.createElement("li");
+
+
+    li.innerHTML = "Task";
+
+    document.body.appendChild(li);
+
+}
